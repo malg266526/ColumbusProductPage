adc67b2da633aab7aafa1a3fa5bcb00d4ba1279a
maltyl <maltyl@clxcommunications.com>
2020-02-16 02:01:46 +0100

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Header } from './modules/header/Header';
-import { Menu } from './modules/navigation/Menu';
+import { NavMenu } from './modules/navigation/Menu';
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <Menu />
+            <NavMenu />
         </div>
     );
 }
--- src/modules/navigation/Menu.tsx
+++ src/modules/navigation/Menu.tsx
@@ -1,33 +1,43 @@
 import React from 'react';
-import { Theme, MenuItem } from '@material-ui/core';
+import { Theme, Link } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { COLORS } from '../../common/consts/colors';
+import { FONT_SIZE, FONT_WEIGHT } from '../../common/consts/fonts';
 
 interface IProps {}
 
 const useStyles = makeStyles((theme: Theme) => ({
     root: {
+        display: 'flex',
         height: 40,
         backgroundColor: COLORS.PINK,
         alignItems: 'center',
         padding: theme.spacing(),
     },
-    menuHorizontal: {
-        display: 'flex',
+    link: {
+        color: 'white',
+        marginRight: theme.spacing(4),
+        fontSize: FONT_SIZE.REGULAR,
+        fontWeight: FONT_WEIGHT.LIGHT,
     },
 }));
 
 const navList = ['All our products', 'Inspiration for the office', 'About OS'];
 
-export const Menu: React.FC<IProps> = () => {
+export const NavMenu: React.FC<IProps> = () => {
     const classes = useStyles();
     return (
         <nav className={classes.root}>
-            <Menu>
-                {navList.map(nav => (
-                    <MenuItem>{nav}</MenuItem>
-                ))}
-            </Menu>
+            {navList.map(nav => (
+                <Link
+                    key={nav}
+                    href={'#'}
+                    variant="body2"
+                    className={classes.link}
+                >
+                    {nav}
+                </Link>
+            ))}
         </nav>
     );
 };
